155a1fb06f2fee237b88cd7a889a59a81e21bab0
Rodrigo <rodrigoderteano@gmail.com>
2012-05-27 19:05:23 -0300

    datea_follow model init

--- datea/datea_follow/models.py
+++ datea/datea_follow/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
+
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes import generic
 
 # Create your models here.
+
+class DateaFollow(models.Model):
+    
+    user = models.ForeignKey(User, related_name="follows")
+    created = models.DateTimeField(_('created'), auto_now_add=True)
+    
+    # generic content type relation to followed object
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    followed_object = generic.GenericForeignKey()
+    
+    class Meta:
+        verbose_name = _('Follow')
+        verbose_name_plural = _('Follows')
+        
