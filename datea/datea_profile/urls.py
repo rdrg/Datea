483d0f7132774be3228031ef3e81939d58a25a73
Rodrigo <rodrigoderteano@gmail.com>
2012-08-17 14:31:26 -0300

    user redirect debug

--- datea/datea_profile/urls.py
+++ datea/datea_profile/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns("datea.datea_profile.views",
     url("^edit_profile/notify_settings/", 'redirect_notify_settings'),
-    url("^profile/(?P<id>[0-9]+)/", 'redirect_user'),
-    url("^perfil/(?P<id>[0-9]+)/", 'redirect_user'),
+    url("^profile/(?P<id>[a-zA-Z0-9-_.]+)/", 'redirect_user'),
+    url("^perfil/(?P<id>[a-zA-Z0-9-_.]+)/", 'redirect_user'),
 )
